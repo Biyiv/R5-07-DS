82baffaa6a7e4df87e937e2cd612fa6a2d3948be
Biyiv <130377518+Biyiv@users.noreply.github.com>
2024-11-20 13:56:13 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,19 @@
+public class Q5 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Q5 <number_of_lines>");
+            return;
+        }
+
+        try {
+            int nbEtoiles = Integer.parseInt(args[0]);
+            for (int i = 1; i <= nbEtoiles; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide a valid integer.");
+        }
+    }
+}
